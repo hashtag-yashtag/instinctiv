36165a27c8242a5680ad57876693fbc8beb49992
Shafay Haq <haqs@purdue.edu>
2019-02-12 00:53:53 -0500

    added Firebase signup form

--- instinctiv/src/components/Firebase/firebase.js
+++ instinctiv/src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
     apiKey: "AIzaSyBvSjVcV8LNU63a5BKYuSRNh68G67Upbsk",
@@ -12,7 +13,28 @@ const config = {
   class Firebase {
     constructor() {
       app.initializeApp(config);
+
+      this.auth = app.auth();
+      //doCreateUserWithEmailAndPassword("shafayhaq123@hotmail.com", "ShafayHaq1");
+      //doSignInWithEmailAndPassword("shafayhaq123@hotmail.com", "ShafayHaq1");
     }
+
+      // *** Auth API ***
+
+    doCreateUserWithEmailAndPassword = (email, password) =>
+      this.auth.createUserWithEmailAndPassword(email, password);
+    
+      
+    doSignInWithEmailAndPassword = (email, password) =>
+      this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+      this.auth.currentUser.updatePassword(password);
+
   }
   
   export default Firebase;
