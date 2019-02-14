a27a2a79e7f4018c97afc7286d50b7865dc9c3f7
Jade Fisher <fishe203@purdue.edu>
2019-02-14 18:48:26 -0500

    Added password reset and change password pages to Account page.

--- instinctiv/src/components/Account/index.js
+++ instinctiv/src/components/Account/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { PasswordForgetForm } from '../PasswordForget';
+import { PasswordChangeForm } from '../PasswordChange';
 
 
 const Account = () => (
   <div>
     Account
     <PasswordForgetForm/>
+    <PasswordChangeForm/>
   </div>
 );
 
