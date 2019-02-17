d1cf5c315d95c41f743758a499fdcbe50ccb9694
Yash Gupta <yash@Yashs-MacBook-Pro-2.local>
2019-02-16 20:17:20 -0500

    Added very basic AlphaVantage API Integration in server/index.js

--- instinctiv/src/components/Home/index.js
+++ instinctiv/src/components/Home/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import { render } from 'react-dom'
+import Highcharts from 'highcharts/highstock'
+import HighchartsReact from 'highcharts-react-official'
 
 const Home = () => (
   <div>
     Home
+
   </div>
 );
 
