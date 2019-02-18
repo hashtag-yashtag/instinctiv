3346777cd8f31e016f403480f4235d27bf59457f
Jade Fisher <fishe203@purdue.edu>
2019-02-18 17:42:59 -0500

    Moved Password forget to signup and removed it from account

--- instinctiv/src/components/Account/index.js
+++ instinctiv/src/components/Account/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
 
 
 const Account = () => (
   <div>
     <h1>Account</h1>
-    <PasswordForgetForm />
     <PasswordChangeForm />
   </div>
 );
