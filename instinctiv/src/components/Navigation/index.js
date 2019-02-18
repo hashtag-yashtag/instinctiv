ece9b257b243c5917f29fe51a0d7721eb1b98fc7
tanuj1998 <tyadav@purdue.edu>
2019-02-18 18:35:25 -0500

    foundationd works now

--- instinctiv/src/components/Navigation/index.js
+++ instinctiv/src/components/Navigation/index.js
@@ -5,26 +5,28 @@ import * as ROUTES from '../../constants/routes';
 
 const Navigation = () => (
   <div>
-    <ul>
+    <div div className="top-bar-right">
+    <ul className="vertical medium-horizontal menu">
       <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+        <Link to={ROUTES.SIGN_IN} className="menu-text1" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Sign In</Link>
       </li>
       <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
+        <Link to={ROUTES.LANDING} className="menu-text1" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Landing</Link>
       </li>
       <li>
-        <Link to={ROUTES.HOME}>Home</Link>
+        <Link to={ROUTES.HOME} className="menu-text1" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Home</Link>
       </li>
       <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
+        <Link to={ROUTES.ACCOUNT} className="menu-text1" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Account</Link>
       </li>
       <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
+        <Link to={ROUTES.ADMIN} className="menu-text1" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Admin</Link>
       </li>
       <li>
         <SignOutButton />
       </li>
     </ul>
+    </div>
   </div>
 );
 
