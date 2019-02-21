c356a236f12b5001376c9ab16ab5ba9348cf3281
Parthasarthi Taneja <ParthasarthiTaneja@parthasarthis-MacBook-Pro.local>
2019-02-21 01:05:37 -0500

    highcharts try

--- instinctiv/src/index.js
+++ instinctiv/src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import Firebase, { FirebaseContext } from './components/Firebase';
 
-/* import Highcharts from 'highcharts';
+import Highcharts from 'highcharts';
 
 class Chart extends Component {
   constructor(props) {
