db44897d6774e5080d10531d35885ca0679076da
Shafay Haq <haqs@purdue.edu>
2019-02-20 21:00:09 -0500

    imlemented auth Nav bar

---
+++ instinctiv/src/components/Navigation/NavigationAuth.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SignOutButton from '../SignOut';
+import * as ROUTES from '../../constants/routes';
+
+
+/* export default withAuthentication(AppS);
+ */
+
+const NavigationAuth = () => (
+  <div>
+    <div div className="top-bar-right">
+    
+    <ul className="vertical medium-horizontal menu">
+      <li>
+        <Link to={ROUTES.HOME} className="menu-text1" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Home</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.ACCOUNT} className="menu-text1" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Account</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.ADMIN} className="menu-text1" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Admin</Link>
+      </li>
+      <li>
+        <SignOutButton />
+      </li>
+    </ul>
+    </div>
+  </div>
+);
+
+export default NavigationAuth;
--- instinctiv/src/components/Navigation/index.js
+++ instinctiv/src/components/Navigation/index.js
@@ -1,8 +1,8 @@
   import React from 'react';
-import { Link } from 'react-router-dom';
-import SignOutButton from '../SignOut';
-import * as ROUTES from '../../constants/routes';
 import { AuthUserContext } from '../Session';
+import NavigationAuth from './NavigationAuth';
+import NavigationNonAuth from './NavigationNonAuth';
+
 
 
 /* export default withAuthentication(AppS);
@@ -10,28 +10,11 @@ import { AuthUserContext } from '../Session';
 
 const Navigation = () => (
   <div>
-    <div div className="top-bar-right">
-    <ul className="vertical medium-horizontal menu">
-      <li>
-        <Link to={ROUTES.SIGN_IN} className="menu-text1" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.LANDING} className="menu-text1" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HOME} className="menu-text1" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT} className="menu-text1" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Account</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ADMIN} className="menu-text1" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Admin</Link>
-      </li>
-      <li>
-        <SignOutButton />
-      </li>
-    </ul>
-    </div>
+    <AuthUserContext.Consumer>
+      {authUser =>
+        authUser ? <NavigationAuth /> : <NavigationNonAuth />
+      }
+    </AuthUserContext.Consumer>
   </div>
 );
 
