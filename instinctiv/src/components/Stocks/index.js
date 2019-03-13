2138cc1e3aafe71eb79787697bedae2e697a99ac
Jade Fisher <fishe203@purdue.edu>
2019-03-13 18:09:26 -0500

    Adding functionality it is somewhat working

--- instinctiv/src/components/Stocks/index.js
+++ instinctiv/src/components/Stocks/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { AuthUserContext, withAuthorization } from '../Session';
 import { Input, Button } from 'reactstrap';
+import './stocks.css';
 
 
 class Stocks extends Component {
@@ -53,7 +54,12 @@ class Stocks extends Component {
           <h9> number of tokens.</h9>
           <h9> </h9>
           <h4> Search Stock News</h4>
+          
+          <input type="text" id="compName" placeholder="Company Name"></input>
+          
+          <button onClick="viewNews()">Search</button>
           <div id="news"></div>
+          
           </div>
         </div>
 
@@ -118,50 +124,54 @@ class Stocks extends Component {
     const { balance } = this.state;
     return balance === 0 ? "Zero" : balance;
   }
+
+  viewNews() {
+     //document.getElementById('news').innerHTML ='No News';
+     var stock = "Apple"
+     var url = 'https://newsapi.org/v2/everything?q='
+               + stock  +
+               '&apiKey=34c665fbab834d7c80356f0bf458b1a7';
+   
+     fetch(url)
+       .then(response => response.json())
+       .then(data => {
+         console.log(data);
+   
+       for(var i=0; i < 12; i++){
+   
+         var title = data.articles[i].title;
+   
+         if(title == null){
+           title ="";
+         }
+         var desc = data.articles[i].description;
+         var auth = data.articles[i].author;
+         var link = data.articles[i].url;
+         var link1 = data.articles[i].urlToImage;
+         var date = data.articles[i].publishedAt;
+   
+   
+   
+   
+       document.getElementById('news').innerHTML += '<div class="item"><h2 class="header">' + title + '</h2>' +
+                  //character of escape: "quotes" and '+'
+         '<img src="' + link1 +'">' +
+         '<p class="publishedAt">' + date + '</p>' +
+         '<p>' + desc + '</p>' +
+         '<p>' + auth + '</p>' +
+                  //character of escape: "quotes" and '+'
+         '<a href="'+ link +'">Read more</a></div>'
+         ;
+         
+     }
+     });
+   
+   }
+
 }
-//viewNews();
-function viewNews() {
-  document.getElementById('news').innerHTML ='';
-  var stock = "Microsoft";
-  var url = 'https://newsapi.org/v2/everything?q='
-            + stock  +
-            '&apiKey=34c665fbab834d7c80356f0bf458b1a7';
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-
-    for(var i=0; i < 12; i++){
-
-      var title = data.articles[i].title;
-
-      if(title == null){
-        title ="";
-      }
-      var desc = data.articles[i].description;
-      var auth = data.articles[i].author;
-      var link = data.articles[i].url;
-      var link1 = data.articles[i].urlToImage;
-      var date = data.articles[i].publishedAt;
 
 
 
-
-    document.getElementById('news').innerHTML += '<div class="item"><h2 class="header">' + title + '</h2>' +
-               //character of escape: "quotes" and '+'
-      '<img src="' + link1 +'">' +
-      '<p class="publishedAt">' + date + '</p>' +
-      '<p>' + desc + '</p>' +
-      '<p>' + auth + '</p>' +
-               //character of escape: "quotes" and '+'
-      '<a href="'+ link +'">Read more</a></div>'
-      ;
-      
-  }
-});
-
-}
 const condition = authUser => !!authUser;
 
 export default withAuthorization(condition)(Stocks);
