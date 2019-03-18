076ffba5f518e24d7ca25093b132763069741f74
varunkondapalli <kondapal@purdue.edu>
2019-03-17 22:00:45 -0400

    Themes

---
+++ instinctiv/src/components/App/Sky.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Sky = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: ${props => props.theme.skyColor}
+`;
+
+export default Sky;
--- instinctiv/src/components/App/index.js
+++ instinctiv/src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navigation from "../Navigation";
@@ -20,8 +20,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "foundation-sites/dist/css/foundation.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Title from './Title';
+import { ThemeProvider } from 'styled-components';
+import Sky from './Sky';
+import CelestialObject from './CelestialObject';
 
-const App = () => (
+const dayTheme = {
+  skyColor: '#37d8e6',
+  celestialObjectColor: '#ffdd00',
+  celestialObjectBorderColor: '#f1c40f'
+};
+
+const nightTheme = {
+  skyColor: '#2c3e50',
+  celestialObjectColor: '#bdc3c7',
+  celestialObjectBorderColor: '#eaeff2'
+}
+
+const Pages = () => (
+
   <Router>
     <div>
       <Navigation />
@@ -40,5 +57,44 @@ const App = () => (
     </div>
   </Router>
 );
+
+class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+
+
+    // Initial state: day time!
+    this.state = {
+      isDay: true,
+      theme: dayTheme
+
+    };
+  }
+
+  handleClick() {
+    // Toggle day / night on click
+    const isDay = !this.state.isDay;
+
+    this.setState({
+      isDay: isDay,
+      theme: isDay ? dayTheme : nightTheme
+
+    });
+  }
+
+  render() {
+    // Wrap the entire content in a <ThemeProvider>
+    return <ThemeProvider theme={this.state.theme}>
+        <Sky>
+          <Title>{this.state.title}</Title>
+          <CelestialObject
+            onClick={() => this.handleClick()}>
+          </CelestialObject>
+        </Sky>
+    </ThemeProvider>
+  }
+}
+
 
 export default withAuthentication(App);
