012ced0a331c2bd462199afc040ee76ab8bf09e9
Shafay Haq <haqs@purdue.edu>
2019-03-21 01:56:04 -0400

    removed view news

--- instinctiv/src/components/Home/index.js
+++ instinctiv/src/components/Home/index.js
@@ -98,49 +98,6 @@ function viewNews(ticker) {
   });
 } */
 
-function viewNews(ticker) {
-  document.getElementById('news').innerHTML ='';
-  var stock = ticker;
-  var url = 'https://newsapi.org/v2/everything?q='
-            + stock  +
-            '&apiKey=34c665fbab834d7c80356f0bf458b1a7';
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-
-    for(var i=0; i < 12; i++){
-
-      var title = data.articles[i].title;
-
-      if(title == null){
-        title ="";
-      }
-      var desc = data.articles[i].description;
-      var auth = data.articles[i].author;
-      var link = data.articles[i].url;
-      var link1 = data.articles[i].urlToImage;
-      var date = data.articles[i].publishedAt;
-/*
-      var child = document.createElement('div');
-      child.innerHTML */
-
-
-
-    document.getElementById('news').innerHTML += '<div class="item"><h2 class="header">' + title + '</h2>' +
-               //character of escape: "quotes" and '+'
-      '<img src="' + link1 +'">' +
-      '<p class="publishedAt">' + date + '</p>' +
-      '<p>' + desc + '</p>' +
-      '<p>' + auth + '</p>' +
-               //character of escape: "quotes" and '+'
-      '<a href="'+ link +'">Read more</a></div>'
-      ;
-      }
-  });
-}
-
 class HomePage extends Component {
 
   constructor(){
