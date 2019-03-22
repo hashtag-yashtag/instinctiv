10058b21b8cda0a81de5c082c75a7f83b2671a04
Parthasarthi Taneja <ParthasarthiTaneja@parthasarthis-MacBook-Pro.local>
2019-03-22 03:45:59 -0400

    landing edit 1

--- instinctiv/src/components/Landing/index.js
+++ instinctiv/src/components/Landing/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
+import Parth_104 from "./Parth_104.png";
+import Parth_102 from "./Parth_102.png";
+import Parth_103 from "./Parth_103.png";
+import Parth_101 from "./Parth_101.png";
 
+function Header() {
+  return (
+    <div>
+      <img src={Parth_104} alt="Parth_104" />
+      <img src={Parth_102} alt="Parth_102" />
+      <img src={Parth_103} alt="Parth_103" />
+      <img src={Parth_101} alt="Parth_101" />
+    </div>
+  );
+}
 
-const Landing = () => (
-  <div>
-    Landing
-  </div>
-);
-
-export default Landing;
+export default Header;
