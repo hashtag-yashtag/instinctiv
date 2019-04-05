4cdfcf902aa138efd9308f6b5cc7e1aeac651eaa
tanuj1998 <tyadav@purdue.edu>
2019-04-05 17:07:51 -0400

    added most bet placed table

--- instinctiv/src/components/Admin/index.js
+++ instinctiv/src/components/Admin/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import { AuthUserContext, withAuthorization } from '../Session';
+import {Table, Row, Col} from 'reactstrap';
 
 const Admin = () => (
-  <div className="row">
-    Hi Admin!
+  <div>
+    <Row>
+      <Col sm="8"></Col>
+      <Col sm="4">
+        <h3>Most Betted on</h3>
+    <Table>
+        <thead>
+          <tr>
+            <th>Stocks</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>AAPL</th>
+          </tr>
+          <tr>
+            <th>FB</th>
+          </tr>
+          <tr>
+            <th>MSFT</th>
+          </tr>
+        </tbody>
+      </Table>
+      </Col>
+      </Row>
 </div>
 );
 
