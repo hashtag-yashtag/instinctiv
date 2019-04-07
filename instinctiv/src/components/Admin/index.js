8b5ba7fdb4f0270be8635fbb957a67a651a4b727
tanuj1998 <tyadav@purdue.edu>
2019-04-07 16:53:29 -0400

    admin updated

--- instinctiv/src/components/Admin/index.js
+++ instinctiv/src/components/Admin/index.js
@@ -5,24 +5,28 @@ import {Table, Row, Col} from 'reactstrap';
 const Admin = () => (
   <div>
     <Row>
-      <Col sm="8"></Col>
-      <Col sm="4">
+      <Col sm="9"></Col>
+      <Col sm="3">
         <h3>Most Betted on</h3>
     <Table>
         <thead>
           <tr>
             <th>Stocks</th>
+            <th>Number</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <th>AAPL</th>
+            <th>100</th>
           </tr>
           <tr>
             <th>FB</th>
+            <th>78</th>
           </tr>
           <tr>
             <th>MSFT</th>
+            <th>67</th>
           </tr>
         </tbody>
       </Table>
