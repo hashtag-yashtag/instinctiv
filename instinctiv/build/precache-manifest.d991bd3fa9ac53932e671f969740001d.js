a8f9020ed87f70fedfd475c406fd417d73dfd2ae
Yash Gupta <ygupta96@gmail.com>
2019-04-24 18:24:12 -0400

    FIX

---
+++ instinctiv/build/precache-manifest.d991bd3fa9ac53932e671f969740001d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "4606b342823e26f7b447a54b51264138",
+    "url": "/static/media/MC103.4606b342.png"
+  },
+  {
+    "revision": "f1f5527c7c9dfdb237ed",
+    "url": "/static/css/main.bdb9230f.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "9cf42fb90d57683d7d7346a9fae5505b",
+    "url": "/static/media/tokensbuy.9cf42fb9.png"
+  },
+  {
+    "revision": "24947a3a171b89c9ea24",
+    "url": "/static/js/2.1b4765f7.chunk.js"
+  },
+  {
+    "revision": "ddbdfce5cb34456005cd1ac8b384b8a9",
+    "url": "/static/media/logotrial.ddbdfce5.png"
+  },
+  {
+    "revision": "4adcd02ef6979f06364e77514f86a9c1",
+    "url": "/static/media/MC104.4adcd02e.png"
+  },
+  {
+    "revision": "f6e690dd72f69d87305934dc79aca0d8",
+    "url": "/static/media/MC102.f6e690dd.png"
+  },
+  {
+    "revision": "f33f2ac268395be5638afaf5cc39fe4a",
+    "url": "/static/media/MC106.f33f2ac2.png"
+  },
+  {
+    "revision": "fa5642680159010f74730ee56a4ae84c",
+    "url": "/static/media/MC107.fa564268.png"
+  },
+  {
+    "revision": "f1f5527c7c9dfdb237ed",
+    "url": "/static/js/main.319db307.chunk.js"
+  },
+  {
+    "revision": "4c07733992864aaccb89a6a3fad76eec",
+    "url": "/static/media/MC101.4c077339.png"
+  },
+  {
+    "revision": "5f3fa7e95e33f6ce96e84b00eed108e6",
+    "url": "/static/media/MC105.5f3fa7e9.png"
+  },
+  {
+    "revision": "c349e0d9a4d4422bcaa17d6bc78ef0c6",
+    "url": "/static/media/stockspopover.c349e0d9.png"
+  },
+  {
+    "revision": "0ef88e2a2decdd2839c078416bf8cfcb",
+    "url": "/static/media/homestocks.0ef88e2a.png"
+  },
+  {
+    "revision": "34c33d2aa6f9db7e5b8af46b6ecc0641",
+    "url": "/static/media/newsstocks.34c33d2a.png"
+  },
+  {
+    "revision": "db8b72443e90a2141766439f64187359",
+    "url": "/static/media/userprofilepopover.db8b7244.png"
+  },
+  {
+    "revision": "19cb82cad1fcc50569b1f82a85043301",
+    "url": "/static/media/homenewfeed.19cb82ca.png"
+  },
+  {
+    "revision": "3f99e0fef9bd8183ece8786ce4bfffe9",
+    "url": "/static/media/phonepop.3f99e0fe.png"
+  },
+  {
+    "revision": "24947a3a171b89c9ea24",
+    "url": "/static/css/2.4703d4f3.chunk.css"
+  },
+  {
+    "revision": "574133a77ee5d1c54452e3655911b78d",
+    "url": "/index.html"
+  }
+];
