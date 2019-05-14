de747200940d72e830636b5515a78cc7151d15f9
Yash Gupta <ygupta96@gmail.com>
2019-05-14 17:39:31 -0400

    Update firebase.js

--- instinctiv/src/components/Firebase/firebase.js
+++ instinctiv/src/components/Firebase/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const config = {
-    apiKey: "AIzaSyBvSjVcV8LNU63a5BKYuSRNh68G67Upbsk",
-    authDomain: "project-instinctiv.firebaseapp.com",
-    databaseURL: "https://project-instinctiv.firebaseio.com",
-    projectId: "project-instinctiv",
-    storageBucket: "project-instinctiv.appspot.com",
-    messagingSenderId: "438722528457"
+    
   };
 
   class Firebase {
